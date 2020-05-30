9d3b023dd1cbb3ab70f6868d710adcfde80e2515
unknown <rohit.singhwd1993@gmail.com>
2020-05-31 01:07:57 +0530

    progress bar, toaster and froala text-editor added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-top-right',
+      enableHtml: true,
+      progressBar: true,
+      tapToDismiss: true,
+      // toastClass: 'need to be implemented'
+    }),
+    LoadingBarModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
